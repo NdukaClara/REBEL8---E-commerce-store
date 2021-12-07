29fcaf28f39dfe5761f8dc6d267e63fdce9b6c1f
Clara <ndukaclara@gmail.com>
2021-12-07 21:22:59 +0100

    first commit

--- home.js
+++ home.js
@@ -92,7 +92,7 @@ featuredProductsWrapper.forEach((item, i) => {
 
 // js for deal of the week countdown
 const countdown = () => {
-  const countDate = new Date("December 10, 2021 00:00:00").getTime();
+  const countDate = new Date("December 31, 2021 00:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
